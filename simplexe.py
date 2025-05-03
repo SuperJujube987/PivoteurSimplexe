cea13669481548095ee13f7c6f5b15a2939caef5
SuperJujube987 <jujube987@hotmail.com>
2025-05-03 13:27:51 -0400

    Update simplexe.py

--- simplexe.py
+++ simplexe.py
@@ -8,23 +8,28 @@ Created on Sat May  3 10:05:17 2025
 from tkinter import *
 from tkinter import ttk
 import numpy as np
+from fractions import Fraction
 
 class Matrix:
-    def __init__(self, rows, cols, values):
-        self.rows = rows
-        self.cols = cols
-        if (values.size < rows*cols): raise ValueError("Pas assez de valeurs pour initialiser la matrice.")
-        if (values.size > rows*cols): raise ValueError("Trop de valeurs pour initialiser la matrice.")
+    def __init__(self, values):
+        self.rows, self.cols = values.shape
         self.values = values
-        
 
 class SimplexTable:
     def __init__(self, constraints, variables, costs, value, values):
         self.constraints = constraints
+        self.variables = variables
+        self.costs = costs
+        self.value = value
+        self.values = values
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
+menu = Tk()
+menu.title("Pivoteur du Simplexe")
+
+frm = ttk.Frame(menu, padding=10)
 frm.grid()
+
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+ttk.Button(frm, text="Quit", command=menu.destroy).grid(column=1, row=1)
+
+menu.mainloop()
